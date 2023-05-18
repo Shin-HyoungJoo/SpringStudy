ed59c00b9afaba1a6d4630f63f3620cba96f3003
Shin <gbz3267@gmail.com>
2023-05-18 15:25:07 +0900

    공부중

--- src/main/java/com/green/board/BoardService.java
+++ src/main/java/com/green/board/BoardService.java
@@ -3,10 +3,13 @@ package com.green.board;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BoardService {
 
-    private final BoardMapper mapper;
+    private final BoardMapper mapper;   //mapper 클래스 변수를 통해 인터페이스 메소드에 접근할 수 있다.
+                                        //오버라이딩?
 
     @Autowired
     public BoardService(BoardMapper mapper) {
@@ -15,7 +18,22 @@ public class BoardService {
 
     public int insBoard(BoardEntity entity) {
         System.out.println("service-insBoard");
-        mapper.insBoard(entity);
-        return 1;
+        return mapper.insBoard(entity);
+    }
+
+    public int updBoard(BoardEntity entity) {
+        return mapper.updBoard(entity);
+    }
+
+    public int delBoard(BoardEntity entity) {
+        return mapper.delBoard(entity);
+    }
+
+    public List<BoardEntity> selBoardAll() {
+        return mapper.selBoardAll();
+    }
+
+    public BoardEntity selBoardById(BoardEntity entity) {
+        return mapper.selBoardById(entity);
     }
 }
