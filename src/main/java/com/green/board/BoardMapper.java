c22b6c62e8680a687c9814a3f0a5a0a054a0268f
신형주 <130746502+Shin-HyoungJoo@users.noreply.github.com>
2023-05-18 15:27:34 +0900

    Add files via upload

--- src/main/java/com/green/board/BoardMapper.java
+++ src/main/java/com/green/board/BoardMapper.java
@@ -2,7 +2,13 @@ package com.green.board;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
-public interface BoardMapper {
-    void insBoard(BoardEntity entity);
+public interface BoardMapper {  //int는 db에서 쿼리실행시 영향 받은 행을 리턴해준다
+    int insBoard(BoardEntity entity);
+    int updBoard(BoardEntity entity);
+    int delBoard(BoardEntity entity);
+    List<BoardEntity> selBoardAll();
+    BoardEntity selBoardById(BoardEntity entity);
 }
